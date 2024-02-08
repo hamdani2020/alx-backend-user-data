2ed7d35ceb0353bcc08296f5d69ada2c2f113907
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-02-08 21:39:47 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ from typing import List
 
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'/g<field>={}'.format(x),
+    'replace': lambda x: r'\g<field>={}'.format(x),
 }
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
