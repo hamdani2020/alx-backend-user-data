c378792c71640f05131f4fb2258924225ccad20c
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-02-15 21:47:13 +0000

    Working basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,9 +3,26 @@
 Flask module to run the api
 """
 
-from flask import Flask
+from flask import Flask, jsonify, abort, request
+from os import getenv
+from flask_cors import (CORS, cross_origin)
+import os
+
+from api.v1.auth.auth import Auth
+from api.v1.views import app_views
+from api.v1.auth.basic_auth import BasicAuth
+
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
