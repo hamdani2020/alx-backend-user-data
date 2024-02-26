e4603b5ff272f4bf63908a09c7e548a28f461648
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-02-26 11:28:45 +0000

    Add new user to databse

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base, User
+
+
+class DB:
+    """
+    DB class
+    """
+
+
+    def __init__(self) -> None:
+        """
+        Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """
+        Momoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        It adds a new user to the database
+        """
+        try:
+            NewUser = User(email=email, hashed_password=hashed_password)
+            self._session.add(NewUser)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            NewUser = None
+        return NewUser
